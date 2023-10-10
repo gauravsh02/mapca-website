aab66dc268384de622394135836f4d11225f09c9
Gaurav Shankar <gauravsh@edmingle.com>
2023-10-10 15:01:58 +0530

    tnc updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,6 @@ export const Footer = () => {
           <span className='text-sm cursor-pointer max-sm:text-center max-sm:block text-grey-300 sm:text-center hover:text-grey-400 dark:hover:text-grey-300 dark:text-grey-400'>© {new Date(). getFullYear()} <a href='https://mapca.in' className=''>MAP CA</a>. All Rights Reserved.</span>
           <ul className='flex flex-wrap mt-3 text-sm smitems-center sm:mt-0 max-sm:justify-center'>
             <li><a className='mr-4 text-grey-300 hover:text-grey-400 dark:hover:text-grey-300 dark:text-grey-400 md:mr-6' href='/privacy'>Privacy Policy</a></li>
-            <li><a className='mr-4 text-grey-300 hover:text-grey-400 dark:hover:text-grey-300 dark:text-grey-400 md:mr-6' href='/terms-condition'>Terms of Service</a></li>
           </ul>
         </div>
       </footer>
